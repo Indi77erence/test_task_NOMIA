f076adae22bbd0a22377eaccdc7c1f7f92d531c6
Indi77erence <134975129+Indi77erence@users.noreply.github.com>
2024-02-23 23:54:43 +0300

    Update service.py

--- src/pages/service.py
+++ src/pages/service.py
@@ -5,6 +5,16 @@ from src.app.models import survey, question as question_tbl
 
 
 async def get_surveys_with_questions(type_of_establishment: str, session: AsyncSession, name_survey: str):
+    """
+	Функция, которая необходима для получения необходимых опросов.
+
+	Принимает 3 аргумента:
+	- session - экземпляр, который обеспечивает асинхронное взаимодействие с БД.
+	- type_of_establishment - тип заведения.
+	- name_survey - название опроса.
+
+	Функция возвращает состояние проверки.
+	"""
     query = select(survey.c.name_survey, question_tbl.c.question_text, question_tbl.c.id).where(
         (survey.c.name_survey.contains(f'{name_survey}')) & (survey.c.type_of_establishment == type_of_establishment)
     ).select_from(survey.join(question_tbl))
